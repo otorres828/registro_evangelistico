6b2c64d724737e791f0912a3978cedbce6a080b3
Oliver Torres <olivertorres1997@gmail.com>
2023-02-12 01:13:12 -0400

    mix

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,10 @@ use App\Http\Livewire\CE;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'visitas-pendientes');
-    // Route::get('panel', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('visitas-pendientes', VP::class)->middleware('auth')->name('visitas_pendientes');
-    Route::get('celulas-evangelisticas', CE::class)->middleware('auth')->name('celulas_evangelisticas');
-    Route::resource('celulas', CelulasController::class)->middleware('auth')->except('show','create')->names('celulas');
-    Route::resource('visitas', VisitaController::class)->middleware('auth')->except('show','create')->names('visitas');
+Route::get('visitas-pendientes', VP::class)->middleware('auth')->name('visitas_pendientes');
+Route::get('celulas-evangelisticas', CE::class)->middleware('auth')->name('celulas_evangelisticas');
+Route::resource('celulas', CelulasController::class)->middleware('auth')->except('show','create')->names('celulas');
+Route::resource('visitas', VisitaController::class)->middleware('auth')->except('show','create')->names('visitas');
+
+
+require_once __DIR__ . '/fortify.php';
