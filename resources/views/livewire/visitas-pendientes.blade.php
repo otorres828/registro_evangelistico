9953d0d44432bd35aced5f8421108272903716c1
Oliver Torres <olivertorres1997@gmail.com>
2023-02-12 01:15:17 -0400

    mix

--- resources/views/livewire/visitas-pendientes.blade.php
+++ resources/views/livewire/visitas-pendientes.blade.php
@@ -1,6 +1,5 @@
 <div
     class="flex flex-col col-span-full sm:col-span-12  bg-white shadow-lg rounded-sm border border-slate-200">
-
     <header class="flex justify-between px-5 py-4 border-b border-slate-100">
         <h2 class="font-semibold text-slate-800 uppercase">Visitas Pendientes</h2>
         <x-modal-agregar-visita :anfitriones="$anfitriones" />
--- resources/views/livewire/vp.blade.php
+++ resources/views/livewire/vp.blade.php
@@ -1,6 +1,5 @@
 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
     <x-app.dashboard :ce="$ce" :cv="$cv" :pv="$pv">
          @livewire('visitas-pendientes')
-    </x-app.dashboard>
-       
+    </x-app.dashboard> 
 </div>
