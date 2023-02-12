9e307dfd4e53e1f961b502cec305e487fb9d4e49
Oliver Torres <olivertorres1997@gmail.com>
2023-02-12 01:02:11 -0400

    mix

--- database/migrations/2022_03_23_163443_create_sessions_table.php
+++ database/migrations/2022_03_23_163443_create_sessions_table.php
@@ -6,20 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-        Schema::create('secciones', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre');
-            $table->unsignedBigInteger('curso_id');
-            $table->foreign('curso_id')->references('id')->on('cursos')->onDelete('cascade');
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->bigIncrements('session_id');
+            $table->string('id');
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        Schema::dropIfExists('secciones');
+        Schema::dropIfExists('sessions');
     }
 };
