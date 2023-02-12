6920b25ff4d3331c515f65ef966dc4336a1bdfec
Oliver Torres <olivertorres1997@gmail.com>
2023-02-12 01:09:04 -0400

    mix

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,8 @@ use App\Http\Livewire\CE;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'visitas-pendientes');
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-
     // Route::get('panel', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('visitas-pendientes', VP::class)->name('visitas_pendientes');
-    Route::get('celulas-evangelisticas', CE::class)->name('celulas_evangelisticas');
-    Route::resource('celulas', CelulasController::class)->except('show','create')->names('celulas');
-    Route::resource('visitas', VisitaController::class)->except('show','create')->names('visitas');
-
-
-});
+    Route::get('visitas-pendientes', VP::class)->middleware('auth')->name('visitas_pendientes');
+    Route::get('celulas-evangelisticas', CE::class)->middleware('auth')->name('celulas_evangelisticas');
+    Route::resource('celulas', CelulasController::class)->middleware('auth')->except('show','create')->names('celulas');
+    Route::resource('visitas', VisitaController::class)->middleware('auth')->except('show','create')->names('visitas');
