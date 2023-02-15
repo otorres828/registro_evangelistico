2056145822a171981109d53bebe8c77ed809ec1f
Oliver Torres <olivertorres1997@gmail.com>
2023-02-15 14:47:07 -0400

    guacharaca

--- resources/views/livewire/celulas-oficiales.blade.php
+++ resources/views/livewire/celulas-oficiales.blade.php
@@ -4,8 +4,10 @@
     <header class="flex justify-between items-start px-5 py-4 border-b border-slate-100">
         <x-celulas-oficiales.modal-agregarcelula />
         <div>
-            <input wire:model="buscar"type="text" class="p-3 sm:p-2 bg-slate-50 text-gray-600 rounded-lg shadow xl:w-96 "
-                placeholder="Buscar...">
+            <x-jet-input wire:model="buscar"  type="text" class="w-full"  placeholder="Buscar..." />                
+
+            {{-- <input wire:model="buscar"type="text" class="p-3 sm:p-2 bg-slate-50 text-gray-600 rounded-lg shadow xl:w-96 "
+                placeholder="Buscar..."> --}}
 
         </div>
     </header>
