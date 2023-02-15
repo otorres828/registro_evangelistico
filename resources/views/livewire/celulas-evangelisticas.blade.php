680d10bb8b74500bf514f7b9c513753bcfc7df86
Oliver English <52586453+otorres828@users.noreply.github.com>
2023-02-15 11:38:52 -0400

    Todate string

--- resources/views/livewire/celulas-evangelisticas.blade.php
+++ resources/views/livewire/celulas-evangelisticas.blade.php
@@ -62,7 +62,7 @@
                                     <div class="text-center">
                                         @if ($celula->estatus)
                                             <div class="bg-green-500 p-2 font-bold text-white rounded-full text-xs">
-                                                {{ Carbon\Carbon::parse($celula->estatus)->format('d-M') }}
+                                                {{ Carbon\Carbon::parse($celula->estatus)->format('d-M') }} a las {{ Carbon\Carbon::parse($celula->estatus)->toTimeString() }}
                                             </div>
                                         @else
                                             <div class="bg-red-500 p-2 font-bold text-white rounded-full text-xs">
